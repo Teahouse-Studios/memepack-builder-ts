347676eda8c42a3e437bc74eeeb85483fcef862a
MysticNebula70 <34984380+AlPha5130@users.noreply.github.com>
2021-04-22 23:03:18 +0800

    basic content

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @name memepack-builder
+ * @description A resourcepack builder using ts
+ * 
+ * @author MysticNebula70
+ * @license Apache-2.0
+ */
+
+// name
+export const name = 'memepack-builder';
