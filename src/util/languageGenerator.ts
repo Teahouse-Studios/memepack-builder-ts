3880add4086d7b1283a25488b524cabb9b4c3f86
MysticNebula70 <34984380+AlPha5130@users.noreply.github.com>
2021-04-22 23:01:51 +0800

    add util

---
+++ src/util/languageGenerator.ts
@@ -0,0 +1,134 @@
+import * as fs from 'fs';
+import { ModuleOverview } from "../types";
+
+export function generateJSON(filePath: string, moduleOverview: ModuleOverview, modules?: string[], modFiles?: string[]): string {
+    return new languageGenerator(filePath, moduleOverview, modules, modFiles).generateJSON();
+}
+
+export function generateJavaLegacy(filePath: string, moduleOverview: ModuleOverview, modules?: string[], modFiles?: string[]): string {
+    return new languageGenerator(filePath, moduleOverview, modules, modFiles).generateJavaLegacy();
+}
+
+export function generateBedrock(filePath: string, moduleOverview: ModuleOverview, modules?: string[]): string {
+    return new languageGenerator(filePath, moduleOverview, modules).generateBedrock();
+}
+
+class languageGenerator {
+    filePath: string;
+    moduleOverview: ModuleOverview;
+    modules?: string[];
+    modFiles?: string[];
+
+    constructor(filePath: string, moduleOverview: ModuleOverview, modules?: string[], modFiles?: string[]) {
+        this.filePath = filePath;
+        this.moduleOverview = moduleOverview;
+        this.modules = modules;
+        this.modFiles = modFiles;
+    }
+
+    getContent(): Record<string, string> {
+        let content: Record<string, string> = {};
+        fs.readFile(this.filePath, { flag: 'r', encoding: 'utf8' }, (_, data) => {
+            if (this.filePath.endsWith('.json')) {
+                content = JSON.parse(data);
+            }
+            else if (this.filePath.endsWith('.lang')) {
+                content = this.langToJSON(data);
+            }
+        });
+        return content;
+    }
+
+    mergeModules(content: Record<string, string>): Record<string, string> {
+        const modules = this.modules || [];
+        const modulePath = this.moduleOverview.modulePath;
+        for (const module of modules) {
+            const addFile = `${modulePath}/${module}/add.json`;
+            fs.stat(addFile, (_, stats) => {
+                if (stats.isFile()) {
+                    fs.readFile(addFile, { encoding: 'utf8' }, (_, data) => {
+                        const addContent = JSON.parse(data);
+                        for (const k in addContent) {
+                            content[k] = addContent[k];
+                        }
+                    });
+                }
+            });
+            const removeFile = `${modulePath}/${module}/remove.json`;
+            fs.stat(removeFile, (_, stats) => {
+                if (stats.isFile()) {
+                    fs.readFile(removeFile, { encoding: 'utf8' }, (_, data) => {
+                        const removeContent = JSON.parse(data);
+                        for (const k in removeContent) {
+                            delete content[removeContent[k]];
+                        }
+                    });
+                }
+            });
+        }
+        return content;
+    }
+
+    mergeMods(content: Record<string, string>): Record<string, string> {
+        if (this.modFiles) {
+            for (const mod of this.modFiles) {
+                let modContent: Record<string, string> = {};
+                fs.readFile(`${mod}`, { encoding: 'utf8' }, (_, data) => {
+                    if (mod.endsWith('.lang')) {
+                        modContent = this.langToJSON(data);
+                    }
+                    if (mod.endsWith('.json')) {
+                        modContent = JSON.parse(data);
+                    }
+                });
+                for (const k in modContent) {
+                    content[k] = modContent[k];
+                }
+            }
+        }
+        return content;
+    }
+
+    generateJSON() {
+        const content = this.getContent();
+        return this.ensureAscii(JSON.stringify(this.mergeModules(content)));
+    }
+
+    generateJavaLegacy() {
+        const content = this.getContent();
+        return this.JSONToLang(this.mergeModules(content));
+    }
+
+    generateBedrock() {
+        const content = this.getContent();
+        return this.JSONToLang(this.mergeModules(content)).replace(/\n/g, '\t#\n');
+    }
+
+    langToJSON(content: string) {
+        const result: Record<string, string> = {};
+        content.replace(/\r\n/g, '\n').split('\n').forEach((value) => {
+            if (value.trim() !== '' && !value.startsWith('#')) {
+                const keyValuePair = value.split('=');
+                result[keyValuePair[0].trim()] = keyValuePair[1].trim();
+            }
+        });
+        return result;
+    }
+
+    JSONToLang(object: Record<string, string>) {
+        const arr = [];
+        for (const k in object) {
+            arr.push(`${k}=${object[k]}\n`);
+        }
+        return arr.join('\n');
+    }
+
+    ensureAscii(str: string) {
+        const arr: string[] = [];
+        for (let i = 0; i < str.length; i++) {
+            const code = str.charCodeAt(i);
+            arr[i] = code < 128 ? str[i] : `\\u${code.toString(16)}`;
+        }
+        return arr.join('');
+    }
+}
