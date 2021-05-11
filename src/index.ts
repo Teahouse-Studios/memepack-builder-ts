6d12f77ee5a9307a932dc7db693644fd5420c143
MysticNebula70 <34984380+AlPha5130@users.noreply.github.com>
2021-05-11 22:37:31 +0800

    export a useless wrapper class

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,42 @@
  * @license Apache-2.0
  */
 
-import { resolve } from 'path';
-import { bedrockBuilder } from './builder/bedrockBuilder';
-import { javaBuilder } from './builder/javaBuilder';
-import { moduleChecker } from './moduleChecker/base';
-import * as types from './types';
+import { BedrockBuilder } from './builder/bedrockBuilder';
+import { JavaBuilder } from './builder/javaBuilder';
+import { ModuleChecker } from './moduleChecker/base';
+import { BuildOptions } from './types';
 
 // name
 export const name = 'memepack-builder';
-export { bedrockBuilder } from './builder/bedrockBuilder';
-export { javaBuilder } from './builder/javaBuilder';
-export { moduleChecker } from './moduleChecker/base';
+export { BedrockBuilder } from './builder/bedrockBuilder';
+export { JavaBuilder } from './builder/javaBuilder';
+export { ModuleChecker } from './moduleChecker/base';
+export * as util from './util';
+
+export class MemepackBuilder {
+    builder: BedrockBuilder | JavaBuilder;
+    moduleChecker: ModuleChecker;
+    log: string[];
+
+    constructor(platform: 'je' | 'be', resourcePath: string, modulePath: string, buildOptions: BuildOptions, modPath?: string) {
+        this.log = []
+        this.moduleChecker = new ModuleChecker(modulePath);
+        const overview = this.moduleChecker.validateModules();
+        this.log.push(...this.moduleChecker.log);
+        switch (platform) {
+            case 'be':
+                this.builder = new BedrockBuilder(resourcePath, overview, buildOptions);
+                break;
+            case 'je':
+            default:
+                modPath = modPath || '';
+                this.builder = new JavaBuilder(resourcePath, overview, modPath, buildOptions);
+                break;
+        }
+    }
+
+    async build(): Promise<void> {
+        await this.builder.build();
+        this.log.push(...this.builder.log);
+    }
+}
