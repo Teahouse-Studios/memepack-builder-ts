4e307b400bc7f9e7d573999c10f80fe9365b01b9
MysticNebula70 <34984380+AlPha5130@users.noreply.github.com>
2021-12-03 22:09:13 +0800

    update to pack_format 8

--- src/constants/defaultConfig.ts
+++ src/constants/defaultConfig.ts
@@ -3,6 +3,6 @@ import { BuilderConfig } from '../types/BuilderConfig'
 export const defaultConfig: BuilderConfig = {
   defaultFileName: 'meme-resourcepack',
   defaultPath: 'builds',
-  latestJEPackFormat: 7,
+  latestJEPackFormat: 8,
   legacyJEPackFormat: 3,
 }
