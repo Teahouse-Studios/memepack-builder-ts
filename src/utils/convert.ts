ccfbf9117de3fe8fbf8156c999c37866e8ea6270
MysticNebula70 <34984380+AlPha5130@users.noreply.github.com>
2022-05-02 18:46:35 +0800

    pipe

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -1,3 +1,5 @@
+import { RawLanguage } from '../types'
+
 export function ensureAscii(value: string): string {
   const arr: string[] = []
   for (let i = 0; i < value.length; i++) {
@@ -10,37 +12,33 @@ export function ensureAscii(value: string): string {
   return arr.join('')
 }
 
-export function javaLangToJSON(content: string): Record<string, string> {
-  const result: Record<string, string> = {}
-  for (let value of content.replace(/\r\n/g, '\n').split('\n')) {
-    value = value.replace(/#.*$/g, '').trimStart()
-    if (value !== '' && value.includes('=')) {
-      const keyValuePair = value.split('=', 2)
-      result[keyValuePair[0]] = keyValuePair[1]
-    }
-  }
-  return result
+export function javaLangToJSON(content: string): RawLanguage {
+  const entries = content
+    .replace(/\r\n/g, '\n')
+    .split('\n')
+    .map((line) => line.replace(/#.*$/g, '').trimStart())
+    .filter((line) => line !== '' && line.includes('='))
+    .map((line) => line.split('=', 2))
+  return Object.fromEntries(entries)
 }
 
-export function bedrockLangToJSON(content: string): Record<string, string> {
-  const result: Record<string, string> = {}
-  for (let value of content.replace(/\r\n/g, '\n').split('\n')) {
-    value = value.replace(/(?:##|\t).*$/g, '').trimStart()
-    if (value !== '' && value.includes('=')) {
-      const keyValuePair = value.split('=', 2)
-      result[keyValuePair[0]] = keyValuePair[1]
-    }
-  }
-  return result
+export function bedrockLangToJSON(content: string): RawLanguage {
+  const entries = content
+    .replace(/\r\n/g, '\n')
+    .split('\n')
+    .map((line) => line.replace(/(?:##|\t#).*$/g, '').trimStart())
+    .filter((line) => line !== '' && line.includes('='))
+    .map((line) => line.split('=', 2))
+  return Object.fromEntries(entries)
 }
 
-export function JSONToJavaLang(obj: Record<string, string>): string {
+export function JSONToJavaLang(obj: RawLanguage): string {
   return Object.entries(obj)
     .map(([key, value]) => `${key}=${value}`)
     .join('\n')
 }
 
-export function JSONToBedrockLang(obj: Record<string, string>): string {
+export function JSONToBedrockLang(obj: RawLanguage): string {
   return Object.entries(obj)
     .map(([key, value]) => `${key}=${value}\t#`)
     .join('\n')
