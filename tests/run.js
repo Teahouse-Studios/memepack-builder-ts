84b254f48597709f47db1d7d7497ffc53a8c0a95
NekoCharm <34984380+AlPha5130@users.noreply.github.com>
2025-03-16 13:27:34 +0800

    chore: update test

--- tests/run.js
+++ tests/run.js
@@ -1,52 +1,46 @@
 const a = await import('../lib/index.js')
-const fs = await import('node:fs')
-const javaParser = new a.ModuleParser()
-javaParser.addSearchPaths('./java/modules')
-javaParser.searchModules().then((m) => {
-  const builder = new a.JavaPackBuilder(m, './java/modules/priority.txt', './java/mappings/al.json')
-  const options = {
-    platform: 'java',
-    type: 'normal',
-    format: 9,
-    compatible: false,
-    hash: false,
-    mod: [],
-    modules: {
-      resource: ['meme_resourcepack', 'a_letter', 'bagify'],
-      collection: ['version_1.18.2'],
-    },
-  }
-  builder.build(options).then((buffer) => {
-    fs.open('./out/java.zip', 'w', (_err, fd) => {
-      fs.write(fd, buffer, () => {
-        fs.close(fd, () => {
-          console.log('generated java.zip')
-        })
-      })
-    })
-  })
-})
-const bedrockParser = new a.ModuleParser()
-bedrockParser.addSearchPaths('./bedrock/modules')
-bedrockParser.searchModules().then((m) => {
-  const builder = new a.BedrockPackBuilder(m, './bedrock/modules/priority.txt')
-  const options = {
-    platform: 'bedrock',
-    type: 'normal',
-    compatible: false,
-    hash: false,
-    modules: {
-      resource: ['meme_resourcepack', 'bagify'],
-      collection: ['sound_modules'],
-    },
-  }
-  builder.build(options).then((buffer) => {
-    fs.open('./out/bedrock.zip', 'w', (_err, fd) => {
-      fs.write(fd, buffer, () => {
-        fs.close(fd, () => {
-          console.log('generated bedrock.zip')
-        })
-      })
-    })
-  })
-})
+const fs = await import('node:fs/promises')
+
+let parser = new a.ModuleParser()
+parser.addSearchPaths('./java/modules')
+let modules = await parser.searchModules()
+let builder = new a.JavaPackBuilder(
+  modules,
+  './java/modules/priority.txt',
+  './java/mappings/al.json'
+)
+const javaOptions = {
+  platform: 'java',
+  type: 'normal',
+  format: 9,
+  compatible: false,
+  hash: false,
+  mod: [],
+  modules: {
+    resource: ['meme_resourcepack', 'a_letter', 'bagify'],
+    collection: ['version_1.18.2'],
+  },
+}
+let buffer = await builder.build(javaOptions)
+let fd = await fs.open('./out/java.zip', 'w')
+await fs.writeFile(fd, javaBuffer)
+console.log('generated java.zip')
+
+parser = new a.ModuleParser()
+parser.addSearchPaths('./bedrock/modules')
+modules = await parser.searchModules()
+builder = new a.BedrockPackBuilder(modules, './bedrock/modules/priority.txt')
+const bedrockOptions = {
+  platform: 'bedrock',
+  type: 'normal',
+  compatible: false,
+  hash: false,
+  modules: {
+    resource: ['meme_resourcepack', 'bagify'],
+    collection: ['sound_modules'],
+  },
+}
+buffer = await builder.build(bedrockOptions)
+fd = await fs.open('./out/bedrock.zip', 'w')
+await fs.write(fd, buffer)
+console.log('generated bedrock.zip')
